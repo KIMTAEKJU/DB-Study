872bd995f047d65192dd759eb50c89483db3e97c
김택주 <@DESKTOP-9I5VERQ>
2019-01-19 16:49:07 +0900

    jhgjhg

--- 서브쿼리 문제.sql
+++ 서브쿼리 문제.sql
@@ -195,10 +195,14 @@ WHERE
     c.부서번호 = d.dept_no;
     
 -- 문제 7
+
 SELECT 
-    a.직책
+    직책
 FROM
     (SELECT 
+        직책, MAX(평균연봉)
+    FROM
+        (SELECT 
         b.title '직책', AVG(a.salary) '평균연봉'
     FROM
         salaries a, titles b, employees c
@@ -207,26 +211,52 @@ FROM
             AND b.emp_no = c.emp_no
             AND a.to_date = '9999-01-01'
             AND b.to_date = '9999-01-01'
-    GROUP BY b.title order by 평균연봉 desc) a limit 0, 1;
+    GROUP BY b.title) a) b;
     
+-- 현재 자신의 매니저보다 높은 연봉을 받고있는 직원은?
 -- 문제 8
-SELECT 
-    *
-FROM
-    dept_manager a,
-    employees b,
-    salaries c
-WHERE
-    a.emp_no = b.emp_no
-        AND b.emp_no = c.emp_no;
-        
-SELECT 
-    *
-FROM
-    dept_manager a,
-    salaries b,
-    dept_emp c
-WHERE
-    a.dept_no = c.dept_no
-        AND b.emp_no = c.emp_no;
 
+SELECT 
+        c.salary
+    FROM
+        dept_manager a, employees b, salaries c
+    WHERE
+        a.emp_no = b.emp_no
+            AND b.emp_no = c.emp_no
+            AND a.to_date = '9999-01-01'
+            AND c.to_date = '9999-01-01';
+            
+SELECT 
+    b.dept_name,
+    b.사원이름,
+    b.salary,
+    a.매니저이름,
+    a.salary
+FROM
+    (SELECT 
+        a.dept_no,
+            c.salary,
+            CONCAT(b.first_name, ' ', b.last_name) '매니저이름'
+    FROM
+        dept_manager a, employees b, salaries c
+    WHERE
+        a.emp_no = b.emp_no
+            AND b.emp_no = c.emp_no
+            AND a.to_date = '9999-01-01'
+            AND c.to_date = '9999-01-01') a,
+    (SELECT 
+        d.dept_name,
+            CONCAT(a.first_name, ' ', a.last_name) '사원이름',
+            b.salary,
+            d.dept_no
+    FROM
+        employees a, salaries b, dept_emp c, departments d
+    WHERE
+        a.emp_no = b.emp_no
+            AND a.emp_no = c.emp_no
+            AND c.dept_no = d.dept_no
+            AND b.to_date = '9999-01-01'
+            AND c.to_date = '9999-01-01') b
+WHERE
+    a.dept_no = b.dept_no
+        AND a.salary < b.salary;        
